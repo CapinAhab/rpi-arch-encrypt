8ebd975e396332d19b4f04b14f5693b3b5eaaa52
CapinAhab <156573833+CapinAhab@users.noreply.github.com>
2025-04-18 16:07:15 +0000

    Mostly working script that needs more testing

---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+#Install dependencies on host system
+xbps-install parted wget cryptsetup xtools binfmt-support
+
+ln -s /etc/sv/binfmt-support /var/service/
+
+wget http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-aarch64-latest.tar.gz
+
+#Need total wipe
+wipefs --all /dev/mmcblk0
+
+parted /dev/mmcblk0 mklabel msdos #Create disk label
+parted /dev/mmcblk0 mkpart primary fat32 1M 257M #Create boot partition, 1M should be a suitable offset for most discs
+parted /dev/mmcblk0 mkpart primary ext4 257M 100%
+
+
+#Setup encrypted root
+cryptsetup luksFormat /dev/mmcblk0p2
+cryptsetup luksOpen /dev/mmcblk0p2 rpiroot
+
+#Setup logical volumes
+vgcreate rpiroot /dev/mapper/rpiroot
+lvcreate --name swap -L 512M rpiroot
+lvcreate --name root -l 100%FREE rpiroot
+
+#Create filesystems 
+mkfs.fat /dev/mmcblk0p1
+mkfs.ext4 /dev/rpiroot/root
+mkswap /dev/rpiroot/swap
+
+#Mount partitions for installation
+mount /dev/rpiroot/root /mnt
+mkdir /mnt/boot
+mount /dev/mmcblk0p1 /mnt/boot
+
+tar xvfp ArchLinuxARM-rpi-aarch64-latest.tar.gz -C /mnt #install system
+rm ArchLinuxARM-rpi-aarch64-latest.tar.gz
+
+#Create fstab
+cp configs/fstab /mnt/etc/fstab
+
+#Cant overwrite resolv.conf so have to remove it
+rm /mnt/etc/resolv.conf
+
+#Copy resolv.conf so chroot has internet
+cp /etc/resolv.conf /mnt/etc/
+
+#Give hooks to mkinitcpio
+cp configs/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
+
+#Setup arch arm repos
+xchroot /mnt pacman-key --init
+xchroot /mnt pacman-key --populate archlinuxarm
+
+
+#Install requirements on the encrypted system
+xchroot /mnt pacman -Syu cryptsetup lvm2
+
+#Setup initramfs
+xchroot mkinitcpio -P
+
+#Setup boot options
+echo "initramfs initrd.img followkernel" >> /mnt/boot/config.txt
+
+
+
+#Unmount partitions
+#umount /mnt/boot
+#umount /mnt
+
+
+#Close luks partition
+#cryptsetup luksClose /dev/mapper/rpiroot
