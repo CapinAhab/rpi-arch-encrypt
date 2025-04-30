eede5db8849cbfca3cc2670715702eebbc6f62ee
CapinAhab <156573833+CapinAhab@users.noreply.github.com>
2025-04-30 06:45:11 +0000

    Script should work, needs to be tested when archarm mirrors are back up

--- install.sh
+++ install.sh
@@ -25,12 +25,6 @@ vgcreate rpiroot /dev/mapper/rpiroot
 lvcreate --name swap -L 1G rpiroot
 lvcreate --name root -l 100%FREE rpiroot
 
-#Get partition uuids
-boot_part=lsblk -o uuid /dev/mmcblk0p1 | sed -n '2p'
-crypt_part=lsblk -o uuid /dev/mmcblk0p2 | sed -n '2p'
-root_part=lsblk -o uuid /dev/rpiroot/swap | sed -n '2p'
-swap_part=lsblk -o uuid /dev/rpiroot/root | sed -n '2p'
-
 #Create filesystems 
 mkfs.fat /dev/mmcblk0p1
 mkfs.btrfs /dev/rpiroot/root
@@ -48,8 +42,6 @@ rm ArchLinuxARM-rpi-aarch64-latest.tar.gz
 echo $(lsblk -o uuid /dev/rpiroot/swap | sed -n '2p')	/              	btrfs      	rw        	0 1 >> /mnt/etc/fstab
 echo $(lsblk -o uuid /dev/rpiroot/root | sed -n '2p')	none           	swap      	defaults  	0 0 >> /mnt/etc/fstab
 
-#echo $boot_part	/boot           	vfat      	rw,defaults  	0 0 >> /mnt/etc/fstab
-
 
 #Update crypttab
 touch /mnt/etc/crypttab
@@ -65,32 +57,32 @@ cp /etc/resolv.conf /mnt/etc/
 cp configs/mkinitcpio.conf /mnt/etc/mkinitcpio.conf
 
 #replace Uboots default envs so vg root partition is used
-sed -i '6s/.*/setenv bootargs console=ttyS1,115200 console=tty0 cryptdevice=PARTUUID=$(lsblk -o uuid /dev/mmcblk0p2 | sed -n '2p') root=PARTUUID=$(lsblk -o uuid /dev/rpiroot/root | sed -n '2p') rw rootwait smsc95xx.macaddr="${usbethaddr}"/' /mnt/boot/boot.scr
-sed -i '6s/.*/setenv bootargs console=ttyS1,115200 console=tty0 cryptdevice=PARTUUID=$(lsblk -o uuid /dev/mmcblk0p2 | sed -n '2p') root=PARTUUID=$(lsblk -o uuid /dev/rpiroot/root | sed -n '2p') rw rootwait smsc95xx.macaddr="${usbethaddr}"/' /mnt/boot/boot.txt
+sed -i '6s/.*/setenv bootargs console=ttyS1,115200 console=tty0 cryptdevice=PARTUUID=$(lsblk -o uuid /dev/mmcblk0p2 | sed -n '2p') root=/dev/mapper/rpiroot-root rw rootwait smsc95xx.macaddr="${usbethaddr}"/' /mnt/boot/boot.scr
+sed -i '6s/.*/setenv bootargs console=ttyS1,115200 console=tty0 cryptdevice=PARTUUID=$(lsblk -o uuid /dev/mmcblk0p2 | sed -n '2p') root=root=/dev/mapper/rpiroot-root rw rootwait smsc95xx.macaddr="${usbethaddr}"/' /mnt/boot/boot.txt
 
 #Setup arch arm repos
 xchroot /mnt pacman-key --init
 xchroot /mnt pacman-key --populate archlinuxarm
 
-#Manually get mirror list to avios script breaking timeout errors
+#Install requirements on the encrypted system
+xchroot /mnt pacman -Syu cryptsetup lvm2 uboot-tools wget
+
+#Manually get mirror list to avoid script breaking timeout errors
 xchroot wget http://mirror.archlinuxarm.org/aarch64/community/community.db -O /var/lib/pacman/sync/community.db
 
-
-#Install requirements on the encrypted system
-xchroot /mnt pacman -Syu cryptsetup lvm2
 
 #Setup initramfs
 xchroot mkinitcpio -P
 
-#Setup boot options
-echo "initramfs initrd.img followkernel" >> /mnt/boot/config.txt
+#Run script to regenerate Uboot
+xchroot bash /boot/mkscr
 
 #replace Uboots default envs so vg root partition is used
 cp configs/boot.txt /mnt/boot/boot.txt
 
 #Unmount partitions
 umount /mnt/boot
-umount /mnt
+umount -l /mnt
 
 
 #Close luks partition
